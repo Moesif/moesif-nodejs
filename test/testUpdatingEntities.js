cc7824671967c25cd1cba197ea8ac2a321e76105
xinghengwang <xing@moesif.com>
2021-08-06 15:59:53 -0700

    promisified all the update entity calls.

--- test/testUpdatingEntities.js
+++ test/testUpdatingEntities.js
@@ -13,7 +13,7 @@ if (RUN_TEST) {
     this.timeout(10000);
 
     var middleWare = moesif({
-      applicationId: "Test",
+      applicationId: "test id",
       debug: true
       // debug: 'instrumentation',
     });
@@ -32,7 +32,6 @@ if (RUN_TEST) {
       // console of companyModel
       console.log(JSON.stringify(companyModel));
     }); // end of it
-
 
     it("update single company", function (done) {
       const singleCamelCasedCompany = {
@@ -53,7 +52,17 @@ if (RUN_TEST) {
       );
     }); // end of it
 
-    it("test update company batch", function (done) {
+    it("update single company promise", function () {
+      const singleCamelCasedCompany = {
+        companyId: "randomId" + Math.random(),
+        ipAddress: "199.2.232.2",
+        companyDomain: "hello.com"
+      };
+
+      return middleWare.updateCompany(singleCamelCasedCompany);
+    }); // end of it
+
+    it("update company batch", function (done) {
       const batchCamelCasedCompany = [
         {
           companyId: "randomId" + Math.random(),
@@ -71,7 +80,7 @@ if (RUN_TEST) {
             name: "stuff"
           }
         }
-      ];;
+      ];
 
       middleWare.updateCompaniesBatch(
         batchCamelCasedCompany,
@@ -85,6 +94,28 @@ if (RUN_TEST) {
       );
     }); // end of it
 
+    it("update company batch promise", function () {
+      const batchCamelCasedCompany = [
+        {
+          companyId: "randomId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyDomain: "twitch.com",
+          metadata: {
+            name: "dude"
+          }
+        },
+        {
+          companyId: "randomId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyDomain: "stuff.com",
+          metadata: {
+            name: "stuff"
+          }
+        }
+      ];
+
+      return middleWare.updateCompaniesBatch(batchCamelCasedCompany);
+    }); // end of it
 
     it("update single user", function (done) {
       const singleCamelCasedUser = {
@@ -93,35 +124,42 @@ if (RUN_TEST) {
         companyId: "helloThere"
       };
 
-      middleWare.updateUser(
-        singleCamelCasedUser,
-        function (err, success) {
-          if (err) {
-            console.log(err);
-          } else {
-          }
-          done();
+      middleWare.updateUser(singleCamelCasedUser, function (err, success) {
+        if (err) {
+          console.log(err);
+        } else {
         }
-      );
+        done();
+      });
+    }); // end of it
+
+    it("update single user promise", function () {
+      const singleCamelCasedUser = {
+        userId: "userId" + Math.random(),
+        ipAddress: "199.2.232.2",
+        companyId: "helloThere"
+      };
+
+      return middleWare.updateUser(singleCamelCasedUser);
     }); // end of it
 
     it("update user batch", function (done) {
       const camelCasedUsersArray = [
         {
-        userId: "userId" + Math.random(),
-        ipAddress: "199.2.232.2",
-        companyId: "helloThere"
-      },
-      {
-        userId: "userId" + Math.random(),
-        ipAddress: "199.2.232.2",
-        companyId: "helloThere",
-        metadata: {
-          "name": 'you',
-          "first_name": "hello"
+          userId: "userId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyId: "helloThere"
+        },
+        {
+          userId: "userId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyId: "helloThere",
+          metadata: {
+            name: "you",
+            first_name: "hello"
+          }
         }
-      }
-    ];
+      ];
 
       middleWare.updateUsersBatch(
         camelCasedUsersArray,
@@ -135,6 +173,25 @@ if (RUN_TEST) {
       );
     }); // end of it
 
-    // }); // end of it
+    it("update user batch promise", function () {
+      const camelCasedUsersArray = [
+        {
+          userId: "userId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyId: "helloThere"
+        },
+        {
+          userId: "userId" + Math.random(),
+          ipAddress: "199.2.232.2",
+          companyId: "helloThere",
+          metadata: {
+            name: "you",
+            first_name: "hello"
+          }
+        }
+      ];
+
+      return middleWare.updateUsersBatch(camelCasedUsersArray);
+    }); // end of it
   }); // end of describe
 } // end of if(RUN_TEST)
