361b2c4d4b6791524140d0879955a102e6a08018
xinghengwang <xing@moesif.com>
2023-07-11 10:16:08 -0700

    create test case

---
+++ test/testConfig.js
@@ -0,0 +1,21 @@
+var moesifapi = require('moesifapi');
+var assert = require('assert');
+var moesifConfigManager = require('../lib/moesifConfigManager');
+
+
+describe('moesif config manager tests', function() {
+  var config = moesifapi.configuration;
+
+  config.ApplicationId = 'Application Id';
+
+  it('can get moesif config manager', function (done) {
+    moesifConfigManager.tryGetConfig();
+    setTimeout(() => {
+      console.log('got config back');
+      console.log(JSON.stringify(moesifConfigManager._config, null, ' '));
+      assert(typeof moesifConfigManager._config === 'object', 'we should have app config back as object');
+      done();
+    }, 1000);
+  });
+});
+
