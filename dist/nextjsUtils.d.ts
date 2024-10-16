223a2061ff956be4a3ce77ba53b33c7b47a8a1a9
xinghengwang <xing@moesif.com>
2024-10-15 19:49:59 -0700

    get ip and update types

--- dist/nextjsUtils.d.ts
+++ dist/nextjsUtils.d.ts
@@ -1,15 +1,3 @@
-export function safeGetNextJsBody(clonedObj: any, options: any): Promise<{
-    body: any;
-    transferEncoding?: undefined;
-} | {
-    body?: undefined;
-    transferEncoding?: undefined;
-} | {
-    body: any;
-    transferEncoding: string;
-}>;
-export function getNextJsIp(request: any): any;
-export function getNextJsFullUrl(request: any): string;
 export function extractNextJsEventDataAndSave({ request, requestTime, response, responseTime, options, saveEvent, blockedBy, }: {
     request: any;
     requestTime: any;
